c65d55ece769a2dbbddfacf23169043690110da4
mckinseyleckenby <leckenbym11@gmail.com>
2021-11-05 09:25:02 -0500

    complete

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState('All');
+//create function
+  function handleCategory(e) {
+    // e.target.value will be the value selected by the user
+    setSelectedCategory(e.target.value);
+  }
+  //now filter to display ONLY SELECTED ITEMS
+  const itemsDisplay = items.filter((item) => {
+    if (selectedCategory === "All") return true;
+
+    return item.category === selectedCategory;
+  });
+
+//{items.map((item) => ) UPDATED WITH CONST SELECTED ITEMS
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleCategory}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +27,10 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+
+        {itemsDisplay.map((item) => (
+
+//{items.map((item) => ) UPDATED WITH CONST SELECTED ITEMS
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
